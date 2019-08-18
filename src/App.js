05aa172420acee9bd6f660ecb1dbcf555e30ae5c
DimasYudhistira91 <dimasmillaty@gmail.com>
2019-08-18 13:00:38 +0700

    learn components and props and state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+
+// Component dan props(properties)
+// function Biodata(props) {
+//   return <span> umurnya {props.age} </span>
+// }
+
+// function Greeting(props) {
+//   return <h1> Hello {props.name} - <Biodata age={props.age}/> </h1>
+// }
+
+// // Menggunakan Class
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <Greeting name='Dimas' age='28'/>
+//         <Greeting name='Nida' age='27'/>
+//       </header>
+//       </div>
+//     );
+//   }
+// }
+
+
+
+// STATE
+
+class Timer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      time : props.start
+    }
+  }
+  
+  // Lifecycle
+  componentDidMount() {
+    this.addInterval = setInterval(() => this.increase(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.addInterval)
+  }
+ 
+  increase() {
+    // update state time setiap detik
+    this.setState((state, props) => ({
+       time : parseInt(this.state.time) + 1
+    }))
+  }
+  render() {
+    return (
+      <div>
+        {this.state.time} detik
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Timer start='0'/>
+        <Timer start='10'/>
+        <Timer start='100'/>
       </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
